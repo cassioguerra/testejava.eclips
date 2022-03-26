697f1fb7a25f7c8d10512a4ec85960af81fa651c
cassio guerra <cassioguerra@live.com>
2022-03-26 20:00:01 -0300

    first commit

---
+++ src/br/com/dio/primeiro_programa.java
@@ -0,0 +1,19 @@
+package br.com.dio;
+
+import br.com.dio.model.GATO;
+
+public class primeiro_programa {
+    public static void main(String[] args) {
+        GATO gato = new GATO();
+        System.out.println(gato);
+
+
+
+
+
+ /*  int a = 5;
+   int b = 10;
+        System.out.println( "hello word" + ("\n") + (a + b));*/
+    }
+}
+
